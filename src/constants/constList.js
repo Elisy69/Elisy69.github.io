74ac242ae2d76fe1de3003f3c0981cedf0979c9d
Elisei <strashevsky.elisey@gmail.com>
2024-02-25 14:00:48 +0300

    hotfix

--- src/constants/constList.js
+++ src/constants/constList.js
@@ -1,6 +1,6 @@
 import CryptoJS from "crypto-js";
 import { getTimestamp } from "../helpers/getTimestamp";
 
-export const URL = "https://api.valantis.store:40000/";
+export const URL = "http://api.valantis.store:40000/";
 export const KEY = CryptoJS.MD5(`Valantis_${getTimestamp()}`).toString();
 export const LIMIT = 50;
